b1a89bb211aa924709ff9acf4bc71c6a86b713ac
Dac Phuong <phuonghd@newit.co.jp>
2021-08-30 20:00:57 +0700

    add project

---
+++ resources/views/admin/components/sidebar.blade.php
@@ -0,0 +1,78 @@
+<div class="app-sidebar sidebar-shadow">
+    <div class="app-header__logo">
+        <div class="logo-src"></div>
+        <div class="header__pane ml-auto">
+            <div>
+                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                    <span class="hamburger-box"><span class="hamburger-inner"></span></span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="app-header__mobile-menu">
+        <div>
+            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                                <span class="hamburger-box">
+                                    <span class="hamburger-inner"></span>
+                                </span>
+            </button>
+        </div>
+    </div>
+    <div class="app-header__menu">
+                        <span>
+                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                                <span class="btn-icon-wrapper">
+                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
+                                </span>
+                            </button>
+                        </span>
+    </div>
+    <div class="scrollbar-sidebar">
+        <div class="app-sidebar__inner">
+            <ul class="vertical-nav-menu mt-5">
+                <li class="mb-2">
+                    <a href="#">
+                        <i class="metismenu-icon fas fa-car"></i>
+                        Elements
+                        <i class="metismenu-state-icon fas fa-caret-up"></i>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="/form">
+                                <i class="metismenu-icon"></i>
+                                Form
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/table">
+                                <i class="metismenu-icon">
+                                </i>Table
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="mb-2">
+                    <a href="#">
+                        <i class="metismenu-icon fas fa-gem"></i>
+                        Components
+                        <i class="metismenu-state-icon fas fa-caret-up"></i>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i class="metismenu-icon">
+                                </i>Tabs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="metismenu-icon">
+                                </i>Accordions
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('admin.master');
+});
+Route::get('/table', function () {
+   return view('admin.template.table');
+});
+Route::get('/form', function () {
+   return view('admin.template.form');
+});
