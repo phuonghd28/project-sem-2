fb0a4c4afccfc77389c3555442dcc6978c5e4646
Dac Phuong <phuonghd@newit.co.jp>
2021-08-31 09:37:00 +0700

    setting client

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,13 @@
+@extends('clients.master')
+@section('title')
+
+@endsection
+@section('custom_css')
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('custom_js')
+@endsection
---
+++ resources/views/clients/master.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @yield('custom_css')
+    <title>@yield('title')</title>
+</head>
+<body>
+<div class="main">
+    <header></header>
+    <div class="banner"></div>
+    <div class="container">
+        @yield('content')
+    </div>
+    <footer></footer>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+@yield('custom_js')
+</body>
+</html>
