07bb50f56c93b190f273ccbe9df6766607bd468d
unknown <tiendvth2009015@fpt.edu.vn>
2021-09-26 01:44:21 +0700

    mensebger

--- resources/views/clients/master.blade.php
+++ resources/views/clients/master.blade.php
@@ -355,6 +355,7 @@
 {{--        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"--}}
 {{--        crossorigin="anonymous"></script>--}}
 {{--<script src="/assets/scripts/index.js"></script>--}}
+<!-- Messenger Plugin chat Code -->
 <script>
     $("#log-form").validate({
         rules: {
@@ -443,6 +444,29 @@
         }
     });
 
+</script>
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "103283908788356");
+    chatbox.setAttribute("attribution", "biz_inbox");
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v12.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 </script>
 </body>
 </html>
