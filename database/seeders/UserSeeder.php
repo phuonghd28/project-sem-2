008269d9fc305a165a6e8df023f29a04301a39d2
Dac Phuong <phuonghd@newit.co.jp>
2021-09-25 20:39:29 +0700

    add config heroku

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+//        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         DB::table('users')->truncate();
         DB::table('users')->insert([
             [
