97323d3d352168e0c62ddcc3ae833681316ac634
Your Name <thangpdth2009031@fpt.edu.vn>
2021-09-29 09:07:34 +0700

    icon-title

--- resources/views/admin/components/head.blade.php
+++ resources/views/admin/components/head.blade.php
@@ -7,6 +7,7 @@
 <meta name="msapplication-tap-highlight" content="no">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="icon" href="/assets/images/icon-logo.png">
 <link href="/assets/css/main.css" rel="stylesheet">
 <style>
     .metismenu-icon {
